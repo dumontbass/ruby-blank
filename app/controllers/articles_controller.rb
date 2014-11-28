87853d44bfd98878e08e79b61856718cac24c12d
pis0195 <guilherme.ribeiro@supero.com.br>
2014-11-28 19:21:10 -0200

    gitignore

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
   def new
-
+    Pessoa.all
   end
 end
--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,4 +1,5 @@
-class Pessoa
-
+class Pessoa < ActiveRecord::Base
+  self.table_name = 'PESSOA'
+  #self.primary_key = "product_id"
 
 end
